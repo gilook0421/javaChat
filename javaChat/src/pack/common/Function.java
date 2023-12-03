146aa04f75f19e042c28ecda55ba51d3bfe424ec
gilook0421 <gilook0421@gmail.com>
2023-12-03 20:33:42 +0900

    test end

---
+++ javaChat/src/pack/common/Function.java
@@ -0,0 +1,12 @@
+package pack.common;
+
+public class Function {
+
+    // 로그인 처리
+    public static final int LOGIN = 100;
+    public static final int MYLOG = 110;
+
+    // 대기실 채팅
+    public static final int WAITCHAT=500;
+    // 방 채팅
+}
